707b898cd5a8910c48c218bfdf6f1da8e400403a
Akshat4321 <123460624+Akshat4321@users.noreply.github.com>
2023-12-23 11:20:54 +0530

    Create ordered set pbds.cpp

---
+++ ordered set pbds.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define INF (int)1e18
+#define endl '\n'
+const int mod = 1000 * 1000 * 1000 + 7;
+const int N = 100005;
+#define f first
+#define s second
+#define rep(i, a, b) for(int i = (a); i < (b); i++)
+#define rrep(i, a, b) for(int i = (a); i > (b); i--)
+#define vi vector<int>
+#define pii pair<int, int>
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+template <class T> using oset = tree<T,null_type,less<T> , rb_tree_tag,tree_order_statistics_node_update>;
+
+void Solve() {
+    
+}
+
+int32_t main() {
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        //cout << "Case #" << i << ": ";
+        Solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
