56ebc75acf52212b3da9889cf679335453000bb7
Akshat4321 <123460624+Akshat4321@users.noreply.github.com>
2024-02-23 03:00:19 +0530

    Update Modular Exponentiation.cpp

--- Modular Exponentiation.cpp
+++ Modular Exponentiation.cpp
@@ -1,9 +1,9 @@
-long long binpow(long long a, long long b) {
+long long binpow(long long a, long long b , long long mod) {
     long long res = 1;
     while (b > 0) {
-        if (b %2 ==1) res = res * a;
+        if (b %2 ==1) res = res * a % mod;
 
-        a = a * a;
+        a = a * a %mod;
         b = b/ 2;
     }
     return res;
